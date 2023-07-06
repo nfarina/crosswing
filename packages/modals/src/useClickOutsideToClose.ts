8d864129abde6575751c7eb534e89d900fb8bf38
Nick Farina <nfarina@gmail.com>
2023-07-05 23:06:53 -0700

    Popups

---
+++ packages/modals/src/useClickOutsideToClose.ts
@@ -0,0 +1,54 @@
+import { MutableRefObject, useContext, useEffect } from "react";
+import { ModalContext } from "./ModalContext.js";
+
+/**
+ * Listens for clicks outside the given container and calls onClose.
+ */
+export function useClickOutsideToClose(
+  onClose: () => any,
+  containerRef: MutableRefObject<HTMLElement | null>,
+  target: MutableRefObject<HTMLElement | null>,
+) {
+  const { modalRoot } = useContext(ModalContext);
+
+  useEffect(() => {
+    function onClick(e: MouseEvent) {
+      const clicked = e.target as Element;
+      const container = containerRef.current;
+      const creator = target.current; // The element that the user originally clicked to show us.
+
+      // Check if you clicked something in our own modal OR a modal *above* us.
+      // For instance, maybe the popup triggered a dialog, and we don't want the
+      // popup to close because that would close the dialog.
+      if (container) {
+        for (
+          let sibling: Element | null = container;
+          !!sibling;
+          sibling = sibling.nextElementSibling
+        ) {
+          if (containsElement(sibling, clicked)) return;
+        }
+      }
+
+      // Check if you clicked the target element itself. We don't want to close
+      // automatically in this case because usually the target element will do
+      // that automatically.
+      if (creator && containsElement(creator, clicked)) return;
+
+      onClose();
+    }
+
+    // Listen for clicks on our modal root, since that's the universe we live in.
+    const el = modalRoot.current?.parentElement;
+    if (!el) return;
+
+    el.addEventListener("click", onClick, true); // useCapture = true
+    return () => el.removeEventListener("click", onClick, true);
+  }, []);
+}
+
+function containsElement(parent: Element, child: Element): boolean {
+  return !!(
+    parent.compareDocumentPosition(child) & Node.DOCUMENT_POSITION_CONTAINED_BY
+  );
+}
