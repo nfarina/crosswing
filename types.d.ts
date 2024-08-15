280ada3e2d1f09a35bb4b97ad9f39e3a27fd2337
Nick Farina <nfarina@gmail.com>
2024-08-15 14:44:12 -0700

    No need for ttf anymore

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,5 @@
 // This allows TypeScript to understand what Vite will do when non-code
 // assets are imported.
-declare module "*.ttf" {
-  const content: string;
-  export default content;
-}
 declare module "*.woff2" {
   const content: string;
   export default content;
