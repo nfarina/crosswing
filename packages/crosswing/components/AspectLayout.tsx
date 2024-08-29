437dc0601ed69c7faf11ce995d4ce7cb04ff0611
Nick Farina <nfarina@gmail.com>
2024-08-29 14:11:37 -0700

    Fix flip-flopping when scroll bars appear

--- packages/crosswing/components/AspectLayout.tsx
+++ packages/crosswing/components/AspectLayout.tsx
@@ -17,24 +17,58 @@ export function AspectLayout({
 }) {
   const ref = useRef<HTMLDivElement | null>(null);
 
+  // Keep track of the last time we resized, and when that happened, so we can
+  // prevent flip-flop behavior (like when our resizing causes scrollbars to
+  // appear, which causes us to resize again, and the scrollbars disappear, and
+  // we keep resizing back and forth). We store only the total area.
+  const lastSizes = useRef<{ area: number; timestamp: number }[]>([]);
+
   useElementSize(
     ref,
     ({ width, height }) => {
       const { current: element } = ref;
       if (!element) return;
 
+      // Clean up old entries in the lastSizes array.
+      lastSizes.current = lastSizes.current.filter(
+        (entry) => Date.now() - entry.timestamp < 500,
+      );
+
+      let newWidth = width;
+      let newHeight = height;
+
       if (width > 0 || height > 0) {
-        // We have a size! So we can adjust our element's width or height, depending
-        // on what's desired.
+        // Adjust the element's width or height depending on the specified aspect ratio
         if (adjust === "width" && height > 0) {
           debug(`Adjusting width for height ${height}`);
-
-          element.style.width = aspect * height + "px";
+          newWidth = height * aspect;
         } else if (adjust === "height" && width > 0) {
           debug(`Adjusting height for width ${width}`);
-
-          element.style.height = width / aspect + "px";
+          newHeight = width / aspect;
         }
+      }
+
+      // What's the current area?
+      const area = newWidth * newHeight;
+
+      // Record the new area.
+      lastSizes.current.push({ area, timestamp: Date.now() });
+
+      // We consider the area to have flip-flopped if we have entries both
+      // larger and smaller than the current area.
+      const isFlopping =
+        lastSizes.current.some((entry) => entry.area > area) &&
+        lastSizes.current.some((entry) => entry.area < area);
+
+      if (isFlopping) {
+        debug("Flopping detected, skipping resize");
+        return;
+      }
+
+      if (adjust === "width") {
+        element.style.width = newWidth + "px";
+      } else if (adjust === "height") {
+        element.style.height = newHeight + "px";
       }
     },
     [aspect],
