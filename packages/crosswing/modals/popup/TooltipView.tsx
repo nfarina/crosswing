ad1ffc68db4bf89e6e64dc1dc58c239240a985f7
Nick Farina <nfarina@gmail.com>
2025-03-05 15:38:35 -0800

    Use TypeScript projects

--- packages/crosswing/modals/popup/TooltipView.tsx
+++ packages/crosswing/modals/popup/TooltipView.tsx
@@ -4,6 +4,11 @@ import { colors } from "../../colors/colors.js";
 import { fonts } from "../../fonts/fonts.js";
 import { PopupView } from "./PopupView.js";
 
+export * from "./getPopupPlacement.js";
+export * from "./PopupView.js";
+export * from "./usePopup.js";
+export * from "./useTooltip.js";
+
 export function TooltipView({
   tooltip: tooltipText,
   target,
